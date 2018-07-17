d4950231e1d6730a4163bd56cc8c1f25bf7b3bf5
Manohar <manohar.chaudhari@globant.com>
2018-07-17 12:03:20 +0530

    two way binding in ReactJs

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,15 +3,24 @@ import React from "react";
 export class Home extends React.Component {
   constructor(props) {
     super();
-   this.state = {
-       age: props.initalAge,
-       status: 0
-   }
+    this.state = {
+      age: props.initialAge,
+      status: 0,
+      homeLink: props.initialChangeLinkName
+    };
   }
   onMakeOlder() {
-   this.setState({
-       age: this.state.age + 3
-   });
+    this.setState({
+      age: this.state.age + 3
+    });
+  }
+  OnChangeLinkName() {
+    this.props.changeLinkName(this.state.homeLink);
+  }
+  onChangeHandler(event) {
+    this.setState({
+      homeLink: event.target.value
+    });
   }
   render() {
     return (
@@ -24,6 +33,22 @@ export class Home extends React.Component {
         <button onClick={() => this.onMakeOlder()} className="btn btn-primary">
           Make me older !
         </button>
+        <hr />
+        <button onClick={this.props.onGreet} className="btn btn-primary">
+          Greet
+        </button>
+        <hr />
+        <input
+          type="text"
+          value={this.state.homeLink}
+          onChange={event => this.onChangeHandler(event)}
+        />
+        <button
+          onClick={this.OnChangeLinkName.bind(this)}
+          className="btn btn-primary"
+        >
+          Change Link Name
+        </button>
       </div>
     );
   }
@@ -31,5 +56,6 @@ export class Home extends React.Component {
 
 Home.propTypes = {
   name: React.PropTypes.string,
-  initalAge: React.PropTypes.number
+  initialAge: React.PropTypes.number,
+  initialChangeLinkName: React.PropTypes.string
 };
--- src/app/index.js
+++ src/app/index.js
@@ -4,19 +4,38 @@ import { render } from "react-dom";
 import { Home } from "./components/Home";
 import { Header } from "./components/Header";
 class App extends React.Component {
-
+  constructor() {
+    super();
+    this.state = {
+      homeLink: "Home"
+    };
+  }
+  onGreet() {
+    alert("Hello!");
+  }
+  onChangeLinkName(newName) {
+    this.setState({
+      homeLink: newName
+    });
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Header />
+            <Header homeLink={this.state.homeLink} />
           </div>
         </div>
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home name= {"Manohar"} initalAge={28} />
-             </div>
+            <Home
+              name={"Manohar"}
+              initialAge={28}
+              onGreet={this.onGreet}
+              changeLinkName={this.onChangeLinkName.bind(this)}
+              initialChangeLinkName= {this.state.homeLink}
+            />
+          </div>
         </div>
       </div>
     );
