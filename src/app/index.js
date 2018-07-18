f8f36ce598ab98b638fce1fb49e86c927f1eecaa
Manohar <manohar.chaudhari@globant.com>
2018-07-18 12:58:39 +0530

    redux basic without reactJS.

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -18,4 +18,73 @@ class App extends React.Component {
     );
   }
 }
-render(<App />, window.document.getElementById("app"));
+render(<App />, window.document.getElementById("app")); */
+
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+const mathReducer = (
+  state = {
+    result: 1,
+    lastValues: []
+  },
+  action
+) => {
+  switch (action.type) {
+    case "ADD":
+      state = {
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
+      break;
+    case "SUBSTRACT":
+      state = {
+        ...state,
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
+      break;
+  }
+  return state;
+};
+const userReducer = (
+  state = {
+    name: "Manohar",
+    age: 31
+  },
+  action
+) => {
+  switch (action.type) {
+    case "SET_NAME":
+      state = {
+        ...state,
+        name: action.payload
+      };
+      break;
+    case "SET_AGE":
+      state = {
+        ...state,
+        age: action.payload
+      };
+      break;
+  }
+  return state;
+};
+const myLogger = store => next => action => {
+  console.log("Logged Action: ", action);
+  next(action);
+};
+const store = createStore(
+  combineReducers({ mathReducer, userReducer }),
+  {},
+  applyMiddleware(createLogger())
+);
+store.subscribe(() => {
+ // console.log("User updated:", store.getState());
+});
+store.dispatch({ type: "ADD", payload: 100 });
+store.dispatch({ type: "ADD", payload: 30 });
+store.dispatch({ type: "SUBSTRACT", payload: 40 });
+store.dispatch({ type: "SET_NAME", payload: "SHRANYA" });
+store.dispatch({ type: "SET_AGE", payload: 2 });
+store.dispatch({ type: "SET_NAME", payload: "VARSHA" });
